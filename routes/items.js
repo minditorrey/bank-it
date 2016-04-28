9ed371e5fc62c72bfc6f3b9613142048b5e20f65
Mindi Torrey <minditorrey@me.com>
2016-04-28 16:44:50 -0700

    add routes and install dependencies

---
+++ routes/items.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', (req, res) => {
+
+	res.send('all the items');
+}
+
+
+
+module.exports = router;
